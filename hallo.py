1b6f580717df4de92300e3f49a0b208c2b1734e5
Aurexis Digital <hashmatrahimisharifi@hotmail.com>
2026-09-18 13:45:29 +0200

    Update hallo.py

--- hallo.py
+++ hallo.py
@@ -1,4 +1,4 @@
 print("Hallo Git")
 print("Dit is mijn tweede versie")
 print("Deze extra zin is voor een extra commit")
-print("Hallo jongens!!!!!!!!!!! nee jongen")
+init("Hallo jongens!!!!!!!!!!! Nee jongen")
